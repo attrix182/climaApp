31fd24258c4e085b32438da069bf40f20282f488
attrix182 <lucho.bernal@hotmail.com>
2022-02-01 15:28:56 -0300

    1 file modified

--- src/app/clima-api.service.ts
+++ src/app/clima-api.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import '../environment';
 
 @Injectable()
 export class ClimaApiService {
   urlApi: string = `api.openweathermap.org/data/2.5/weather?q=`;
 
-  APIkey = '';
+  APIkey = apiKey;
 
   constructor(private http: HttpClient) {}
 
